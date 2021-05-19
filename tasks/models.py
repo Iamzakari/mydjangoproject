99bbbe7bed82f852426ba53e75f677dc91e823ff
Zakari <41507961+Iamzakari@users.noreply.github.com>
2021-05-19 13:21:34 +0200

    Update models.py
    update models.py
    added aone class for form

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class NewTasksForms(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.ImageField(label="Priority", min_value=1, max_value=10)
